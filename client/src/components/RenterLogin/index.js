218aabc5b2e17de774bb9ab70462657fc9a00541
cwibach <cwibach@uwaterloo.ca>
2023-02-11 10:07:20 -0500

    Renamed files

---
+++ client/src/components/RenterLogin/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
+class RenterLogin extends Component {
+    render () {
+        return(
+            <div>
+                Login as a Renter
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(RenterLogin);
