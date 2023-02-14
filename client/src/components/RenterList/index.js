897668200880b3fdfa3a7357bc5c9217e118e143
cwibach <cwibach@uwaterloo.ca>
2023-02-13 21:04:15 -0500

    view list of renters

---
+++ client/src/components/RenterList/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import { AppBar, Toolbar, Box, Button, CssBaseline, ThemeProvider, Grid} from '@mui/material';
+import {appTheme} from "../../themes/theme";
+import {AppPaper} from "../../themes/paper";
+
+const RenterList = ({renters}) => {
+    const [expanded, setExpanded] = React.useState([]);
+
+    const addToExpanded = (renterID) => {
+        let tempExpanded = [...expanded];
+
+        tempExpanded.push(renterID);
+
+        setExpanded(tempExpanded);
+    }
+
+    const removeFromExpanded = (renterID) => {
+        let tempExpanded = [...expanded];
+
+        for (let i = 0; i < tempExpanded.length; i++){
+            if (tempExpanded[i] == renterID) {
+                tempExpanded.splice(i, 1);
+            }
+        }
+
+        setExpanded(tempExpanded);
+    }
+
+    return(
+        <Grid margin={appTheme.spacing(2)}>
+            {renters.map((renter)=> {
+                return(
+                    <Grid item key={renter.id}>
+                        <AppPaper>
+                            <Typography
+                                style={{marginTop: appTheme.spacing(1),
+                                    marginLeft: appTheme.spacing(3)}}
+                                variant="h5"
+                                component="div"
+                                color="inherit"
+                            >
+                                {renter.name}
+                            </Typography>
+
+                            <Typography
+                                style={{marginTop: appTheme.spacing(1),
+                                    marginLeft: appTheme.spacing(5),
+                                    marginBottom: appTheme.spacing(1)}}
+                                variant="subtitle1"
+                                component="div"
+                                color="inherit"
+                            >
+                                Age: {renter.age}, Gender: {renter.gender}
+                            </Typography>
+
+                            {(expanded.includes(renter.id)) ? (<>
+                                <Typography
+                                    style={{marginTop: appTheme.spacing(1),
+                                        marginLeft: appTheme.spacing(5),
+                                        marginBottom: appTheme.spacing(1)}}
+                                    variant="subtitle1"
+                                    component="div"
+                                    color="inherit"
+                                >
+                                    Typical bedtime: {renter.bedtime}, Cooking Frequency: {renter.cooking}, Max Rent: {renter.maxrent}
+                                </Typography>
+
+                                <Typography
+                                    style={{marginTop: appTheme.spacing(1),
+                                        marginLeft: appTheme.spacing(5),
+                                        marginBottom: appTheme.spacing(1)}}
+                                    variant="subtitle1"
+                                    component="div"
+                                    color="inherit"
+                                >
+                                    Phone Num: {renter.phone}, e-mail address: {renter.email}
+                                </Typography>
+
+                                <Button
+                                    onClick={() => removeFromExpanded(renter.id)}
+                                    variant="contained"
+                                    style={{marginTop: appTheme.spacing(1),
+                                        marginLeft: appTheme.spacing(3),
+                                        marginBottom: appTheme.spacing(2)}}>
+                                    Hide Details
+                                </Button>
+                            </>):(<>
+                                <Button
+                                    onClick={() => addToExpanded(renter.id)}
+                                    variant="contained"
+                                    style={{marginTop: appTheme.spacing(1),
+                                        marginLeft: appTheme.spacing(3),
+                                        marginBottom: appTheme.spacing(2)}}>
+                                    Show Details
+                                </Button>
+                            </>)}
+                        </AppPaper>
+                    </Grid>
+                );
+            })}
+        </Grid>
+    );
+}
+
+export default RenterList;
--- client/src/components/SearchRenters/index.js
+++ client/src/components/SearchRenters/index.js
@@ -8,12 +8,67 @@ import Paper from "@material-ui/core/Paper";
 import { AppBar, Toolbar, Box, Button, CssBaseline, ThemeProvider} from '@mui/material';
 import {appTheme} from "../../themes/theme";
 import history from '../Navigation/history';
+import RenterList from '../RenterList/index';
 
-class SearchRenters extends Component {
-    render () {
-        return(
-            <ThemeProvider theme={appTheme}>
-            <CssBaseline enableColorScheme/>
+const SearchRenters = () => {
+    const [renters, setRenters] = React.useState([]);
+
+    const tempRenters = [
+        {id: 1,
+            name: "Renter 1",
+            age: 21,
+            gender: "male",
+            phone: "613-737-1111",
+            email: "my.name@email.com",
+            maxrent: 900,
+            bedtime: "11:15 PM",
+            cooking: "never"
+        },{id: 2,
+            name: "Renter 2",
+            age: 21,
+            gender: "helicopter",
+            phone: "613-737-1111",
+            email: "my.name@email.com",
+            maxrent: 915,
+            bedtime: "11:15 AM",
+            cooking: "always"
+        },{id: 3,
+            name: "Renter 3",
+            age: 21,
+            gender: "none",
+            phone: "613-737-1111",
+            email: "my.name@email.com",
+            maxrent: 930,
+            bedtime: "4:00 PM",
+            cooking: "daily"
+        },{id: 4,
+            name: "Renter 4",
+            age: 21,
+            gender: "all",
+            phone: "613-737-1111",
+            email: "my.name@email.com",
+            maxrent: 945,
+            bedtime: "4:00 AM",
+            cooking: "weekly"
+        },{id: 5,
+            name: "Renter 5",
+            age: 21,
+            gender: "female",
+            phone: "613-737-1111",
+            email: "my.name@email.com",
+            maxrent: 960,
+            bedtime: "11:15",
+            cooking: "what's that?"
+        }
+    ];
+
+    React.useEffect(() => {
+        setRenters(tempRenters);
+    }, [0]);
+
+    return (
+        <ThemeProvider theme={appTheme}>
+            <CssBaseline enableColorScheme />
 
             <AppBar position="static">
                 <Toolbar>
@@ -29,33 +84,33 @@ class SearchRenters extends Component {
                         alignItems="center">
 
                         <Button
-                        color="inherit" 
-                        style={{ cursor: "pointer" }}
-                        onClick={() => history.push('/RenterProfile')}
-                        size='medium'
-                        sx={{p: 5}}>
+                            color="inherit"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => history.push('/RenterProfile')}
+                            size='medium'
+                            sx={{ p: 5 }}>
                             <Typography variant="h5" color="inherit" noWrap>
                                 Profile
                             </Typography>
                         </Button>
 
                         <Button
-                        color="inherit" 
-                        style={{ cursor: "pointer" }}
-                        onClick={() => history.push('/SearchUnits')}
-                        size='medium'
-                        sx={{p: 5}}>
+                            color="inherit"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => history.push('/SearchUnits')}
+                            size='medium'
+                            sx={{ p: 5 }}>
                             <Typography variant="h5" color="inherit" noWrap>
                                 Search Units
                             </Typography>
                         </Button>
 
                         <Button
-                        color="inherit" 
-                        style={{ cursor: "pointer" }}
-                        onClick={() => history.push('/RenterLogin')}
-                        size='medium'
-                        sx={{p: 5}}>
+                            color="inherit"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => history.push('/RenterLogin')}
+                            size='medium'
+                            sx={{ p: 5 }}>
                             <Typography variant="h5" color="inherit" noWrap>
                                 Logout
                             </Typography>
@@ -64,9 +119,11 @@ class SearchRenters extends Component {
                     </Box>
                 </Toolbar>
             </AppBar>
-            </ThemeProvider>
-        );
-    }
+
+            <RenterList renters={renters}/>
+
+        </ThemeProvider>
+    );
 }
 
 export default SearchRenters;
