e3cc3761d0b89ac34eaa0efe23bde4e994518392
owen-sellner <owen.sellner@gmail.com>
2023-02-21 15:31:49 -0500

    added password to config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
-  host    : 'ov-research-4.uwaterloo.ca',
-  user    : '',
-  password: '',
-  database: ''
+  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
+  user    : 'osellner',
+  password: 'PurplePages321',
+  database: 'osellner'
 };
  
 module.exports = config;
