6b878434dd082e538a87682a871e98956de2060b
owen-sellner <owen.sellner@gmail.com>
2023-02-28 19:11:21 -0500

    MUI for Add Posting Page

--- client/src/components/AddUnit/index.js
+++ client/src/components/AddUnit/index.js
@@ -6,7 +6,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import history from '../Navigation/history';
-import { AppBar, Toolbar, Box, Button, CssBaseline, ThemeProvider } from '@mui/material';
+import { AppBar, Toolbar, Box, Button, CssBaseline, ThemeProvider, TextField } from '@mui/material';
 import { appTheme } from "../../themes/theme";
 
 // SERVER MODE
@@ -16,6 +16,18 @@ const serverURL = "";
 
 function AddUnit() {
 
+    // User Id *** Temporary ***
+    const [userID, setUserID] = React.useState(1);
+
+    // Handles submitting the form
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
 
     return (
         <ThemeProvider theme={appTheme}>
@@ -81,6 +93,100 @@ function AddUnit() {
                     </Box>
                 </Toolbar>
             </AppBar>
+
+
+
+            <Box
+                margin={6}
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"flex-start"}
+            >
+                {/* Creates a column grid for the body of the page */}
+                <Grid container
+                    direction="column"
+                    alignItems="center"
+                    style={{ color: "#e6e6e6" }}
+                    justifyContent="center"
+                    textAlign="center"
+                    xs={4}
+                >
+
+                    {/* Page Title */}
+                    <Grid item>
+                        <Typography variant="h3">
+                            <b>Create New Posting</b>
+                        </Typography>
+                        <Box sx={{ display: 'flex', pb: 2 }}></Box>
+                    </Grid>
+
+                    {/* Posting Information Input */}
+                    <Box onSubmit={handleSubmit}>
+                        <TextField
+                            variant="filled"
+                            style={{ background: "#e6e6e6" }}
+                            color="primary"
+                            name="address"
+                            required
+                            fullWidth
+                            id="address"
+                            label="Address"
+                            sx={{ mt: 3, mb: 2 }}
+                            autoFocus
+                        />
+
+                        <TextField
+                            variant="filled"
+                            style={{ background: "#e6e6e6" }}
+                            required
+                            fullWidth
+                            id="rooms"
+                            type="number"
+                            label="Number of Rooms"
+                            name="rooms"
+                            sx={{ mt: 3, mb: 2 }}
+                            color="primary"
+                        />
+
+                        <TextField
+                            variant="filled"
+                            style={{ background: "#e6e6e6" }}
+                            required
+                            fullWidth
+                            id="bathrooms"
+                            type="number"
+                            label="Number of Bathrooms"
+                            name="bathrooms"
+                            sx={{ mt: 3, mb: 2 }}
+                            color="primary"
+                        />
+
+                        <TextField
+                            variant="filled"
+                            style={{ background: "#e6e6e6" }}
+                            required
+                            fullWidth
+                            name="apt_price"
+                            label="Monthly Rent of Apartment"
+                            id="apt_price"
+                            type="number"
+                            sx={{ mt: 3, mb: 2 }}
+                            color="primary"
+                        />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            color="primary"
+                        >
+                            Sign Up
+                        </Button>
+                    </Box>
+                </Grid>
+            </Box>
+
         </ThemeProvider>
     );
 }
