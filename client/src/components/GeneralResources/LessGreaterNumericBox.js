fd9189e36cefe750d77bff862a6ac1585784e5d4
owen-sellner <owen.sellner@gmail.com>
2023-03-21 00:50:47 -0400

    UI search units

--- client/src/components/GeneralResources/LessGreaterNumericBox.js
+++ client/src/components/GeneralResources/LessGreaterNumericBox.js
@@ -8,48 +8,50 @@ const LessGreaterNumericBox = ({ minValue, maxValue, minChange, maxChange, minLa
         <ThemeProvider theme={appTheme}>
             <CssBaseline enableColorScheme />
 
-            <Grid container spacing={2}>
+            <Grid container spacing={2} alignItems={"center"}>
                 <Grid item xs={3}></Grid>
-                <Grid item xs={4}>
+                <Grid item xs={2}>
+                    <Typography>
+                        {minLabel}
+                        <TextField
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start"> {icon} </InputAdornment>
+                            }}
+                            value={minValue}
+                            onChange={minChange}
+                            fullWidth
+                            style={{ backgroundColor: "#ffffff" }}
+                            type="number"
+                        />
 
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography
+                        variant="h6"
+                        textAlign={"center"}
+                    >
+                        {centreLabel}
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography>
+                        {maxLabel}
+                        <TextField
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start"> {icon} </InputAdornment>
+                            }}
+                            value={maxValue}
+                            onChange={maxChange}
+                            fullWidth
+                            style={{ backgroundColor: "#ffffff" }}
+                            type="number"
+                        />
+
+                    </Typography>
                 </Grid>
                 <Grid item xs={3}></Grid>
             </Grid>
-
-            <Box columnGap={2} sx={{
-                display: "flex", flexwrap: 'wrap', p: 1, backgroundColor: 'primary.background',
-                width: 2 / 3, alignItems: 'center', justifyContent: 'space-between', ml: 4, mt: 1
-            }}>
-
-                <TextField
-                    InputProps={{
-                        startAdornment: <InputAdornment position="start"> {icon} </InputAdornment>
-                    }}
-                    value={minValue}
-                    onChange={minChange}
-                    label={minLabel}
-                    sx={{ width: 2 / 7 }}
-                    type="number"
-                />
-
-                <Typography
-                    variant="h6"
-                    gutterBottom
-                >
-                    &lt;= {centreLabel} &lt;=
-                </Typography>
-
-                <TextField
-                    InputProps={{
-                        startAdornment: <InputAdornment position="start"> {icon} </InputAdornment>
-                    }}
-                    value={maxValue}
-                    onChange={maxChange}
-                    label={maxLabel}
-                    sx={{ width: 2 / 7 }}
-                    type="number"
-                />
-            </Box>
         </ThemeProvider>
     )
 }
